0f60c4528de917d05a7e98bce4b8077e844d432a
namancg <naman.c@perfios.com>
2021-10-07 20:00:01 +0530

    REFACTORED: regex pattern to make sure @ is present in email

--- EmailValidator.js
+++ EmailValidator.js
@@ -1,7 +1,13 @@
 let arguments= process.argv;
 arguments.splice(0,2);
+
 let emailRegEx = RegExp('^[a-zA-z][.@a-zA-Z]*$')
-arguments.forEach(element => {
-console.log(element+"",emailRegEx.test(element))
+arguments.forEach(email => {
+console.log(email+"",emailRegEx.test(email))
+ });
+
+emailRegEx = RegExp('^[a-zA-z]*[@][.a-zA-Z]*$')
+arguments.forEach(email => {
+console.log(email+"",emailRegEx.test(email))
  });
 
